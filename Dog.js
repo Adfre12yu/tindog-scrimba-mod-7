61b99dc89bdcd88642fa5b2e2755078f1f33d92f
Melih Yilmaz <melihyilmaz999@gmail,com>
2022-07-15 14:32:24 -0400

    Added functionality

--- Dog.js
+++ Dog.js
@@ -1,1 +1,21 @@
-// Create the Dog class here
+class Dog {
+    constructor(data) {
+        Object.assign(this, data);
+    }
+
+    getDogHtml() {
+        return `<img src="${this.avatar}" alt="" class="main-img">
+                <div class="desc">
+                  <h1>${this.name}, ${this.age}</h1>
+                  <h2>${this.bio}</h2>
+                </div>`;
+    }
+
+    swiped(liked) {
+        return liked
+            ? `<img src="images/badge-like.png" alt="" class="badge">`
+            : `<img src="images/badge-nope.png" alt="" class="badge">`;
+    }
+}
+
+export default Dog;
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Remember to import the data and Dog class!
+import dogsData from "./data.js";
+import Dog from "./Dog.js";
+
+let dogs = dogsData.map((i) => new Dog(i));
+let nextDog = "";
+
+function getNextDog() {
+    if (dogs.length != 0) {
+        nextDog = new Dog(dogs.shift());
+    } else {
+        dogs = dogsData.map((i) => new Dog(i));
+        getNextDog();
+    }
+}
+
+function render() {
+    getNextDog();
+    document.getElementById("main").innerHTML = nextDog.getDogHtml();
+}
+
+function swiped(liked) {
+    document.getElementById("main").innerHTML += nextDog.swiped(liked);
+    setTimeout(render, 1000);
+}
+
+document
+    .getElementById("like-btn")
+    .addEventListener("click", () => swiped(true));
+
+document
+    .getElementById("dislike-btn")
+    .addEventListener("click", () => swiped(false));
+
+render();
